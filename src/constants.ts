363b1a948b4707f6b9b29ab52427d8d8fc601adc
mateie <mateie@gmail.com>
2025-08-13 20:26:20 -0700

    added constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+// Disallowed substrings in usernames
+export const disallowedNameSubstrings = ["@", "#", ":", "```"];
+export const notAllowedNames = ["everyone", "here", "mutualzz"];
+
+// Zero-width and non-rendering character matcher
+export const invisibleCharsRegex = /[\u200B-\u200D\uFEFF]/;
