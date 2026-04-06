25a5ac9d1dc989ae47b201159cdc9b5e1d94a02e
mateie <mateie@gmail.com>
2026-04-06 13:35:05 -0700

    working ion exrepsions

--- src/expressions.ts
+++ src/expressions.ts
@@ -2,12 +2,13 @@ import z from "zod";
 
 export const validateExpressionPutBody = z.object({
     spaceId: z.string().trim().optional(),
-    userId: z.string().trim().optional(),
 
     name: z.string().trim(),
     type: z.string().refine((val) => ["0", "1"].includes(val), {
         error: "Invalid expression type provided",
     }),
+
+    emoji: z.instanceof(Buffer),
 
     crop: z
         .object({
