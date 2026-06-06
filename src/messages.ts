335e96dbd6b26b5100088c7d946140fb07030d51
Azrael <azrael@mutualzz.com>
2026-06-05 22:01:53 -0700

    Some bug fixes and new formatting

--- src/messages.ts
+++ src/messages.ts
@@ -2,24 +2,24 @@ import z from "zod";
 
 // PUT
 export const validateMessageParamsPut = z.object({
-    channelId: z.string({ error: "Invalid Channel ID" }),
+  channelId: z.string({ error: "Invalid Channel ID" }),
 });
 
 export const validateMessageParamsModify = z.object({
-    channelId: z.string({ error: "Invalid Channel ID" }),
-    messageId: z.string({ error: "Invalid Message ID" }),
+  channelId: z.string({ error: "Invalid Channel ID" }),
+  messageId: z.string({ error: "Invalid Message ID" }),
 });
 
 export const validateMessageBodyPut = z.object({
-    content: z
-        .string()
-        .max(2000, "Message content cannot exceed 2000 characters")
-        .trim()
-        .optional(),
-    nonce: z.string().optional(),
+  content: z
+    .string()
+    .max(2000, "Message cannot exceed 2000 characters")
+    .trim()
+    .optional(),
+  nonce: z.string().optional(),
 });
 
 export const validateMessageAckParams = z.object({
-    channelId: z.string({ error: "Invalid Channel ID" }),
-    messageId: z.string({ error: "Invalid Message ID" }),
+  channelId: z.string({ error: "Invalid Channel ID" }),
+  messageId: z.string({ error: "Invalid Message ID" }),
 });
